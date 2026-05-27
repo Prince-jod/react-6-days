b87b3857f4b64f8f33a2b2a51e89309aa1229a2c
Prince-jod <baghelprince164@gmail.com>
2026-05-27 10:26:54 +0530

    App.jsx updated 1st

--- project-App/src/App.jsx
+++ project-App/src/App.jsx
@@ -3,9 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import Card from './component/Card'//yaha humne import kiya hai dusri jsx file ka function use krne k liye 
+import Nav from './component/NavBar'
 const App=()=>{
   return (
-    <h1>hello world!</h1>
+    
+  <div>
+  <Card user="aman"/>
+   <Card user="Happy"/>
+   <Card user="Himanshu"/>
+   <Nav item="NavBar"/>
+  </div>
+  
   )
 };
 
